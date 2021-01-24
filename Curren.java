4696677a88636a8bdd32535dc8e4f88891b2755b
akash1999aug <73059133+akash1999aug@users.noreply.github.com>
2021-01-24 20:40:55 +0530

    Update Curren.java

--- Curren.java
+++ Curren.java
@@ -1,6 +1,6 @@
 package com.example.ATM2;
 
-public class Currency {
+public class Curren {
 
     private int amount = deno100.count * 100 + deno200.count * 200 + deno500.count * 500 + deno2000.count * 2000;
     private static final int INT_2000 = 2000;
